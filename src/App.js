dd5c96cd8783be1d7538087adffb650c4873825e
J. Luis Arredondo <jose.arredondo.a@outlook.com>
2022-08-11 12:01:19 -0500

    Add 'Home' mock page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import Home from './pages/Home';
+// import CarDetail from './pages/CarDetail';
+// import AddCar from './pages/AddCar';
+// import DeleteCar from './pages/DeleteCar';
+// import Rentals from './pages/Rentals';
+// import NewRental from './pages/NewRental';
+
 function App() {
   return (
     <BrowserRouter>
       {/* <Nav /> */}
       <section>
         <Routes>
-          <Route path="/" element="a" />
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/car-detail" element={<CarDetail />} />
+          <Route path="/add-car" element={<AddCar />} />
+          <Route path="/delete-car" element={<DeleteCar />} />
+          <Route path="/rentals" element={<Rentals />} />
+          <Route path="/new-rental" element={<NewRental />} /> */}
         </Routes>
       </section>
     </BrowserRouter>
