db092bc3b60f5ceca4b3a36eb61fd1086c10dc8d
J. Luis Arredondo <jose.arredondo.a@outlook.com>
2022-08-15 14:23:44 -0500

    Add 'CreateRental' API REST

--- src/redux/rental/rentalsReducer.js
+++ src/redux/rental/rentalsReducer.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const baseUrl = 'http://localhost:3000/api/v1/';
 const FETCH_RENTALS = 'FETCH_RENTALS';
-// const ADD_RENTAL = 'ADD_RENTAL';
+const ADD_RENTAL = 'ADD_RENTAL';
 const DELETE_RENTAL = 'DELETE_RENTAL';
 const initialState = [];
 
@@ -11,6 +11,15 @@ export const fetchRentals = () => async (dispatch) => {
   const res = await axios.get(`${baseUrl}users/${user.id}/reservations`);
   dispatch({
     type: FETCH_RENTALS,
+    payload: res.data,
+  });
+};
+
+export const addRental = (rental) => async (dispatch) => {
+  const user = JSON.parse(localStorage.getItem('user'));
+  const res = await axios.post(`${baseUrl}users/${user.id}/reservations`, rental);
+  dispatch({
+    type: ADD_RENTAL,
     payload: res.data,
   });
 };
@@ -29,8 +38,8 @@ const reducer = (state = initialState, action = {}) => {
   switch (action.type) {
     case 'FETCH_RENTALS':
       return action.payload;
-    // case 'ADD_RENTAL':
-    //   return [...state, action.payload];
+    case 'ADD_RENTAL':
+      return [...state, action.payload];
     case 'DELETE_RENTAL':
       return state.filter((rental) => rental.id !== action.element);
     default:
