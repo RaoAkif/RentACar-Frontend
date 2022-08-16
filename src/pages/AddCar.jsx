4e105b6a34bc917c13ab617fa63d11af8e735b59
raoakif <akifrao@gmail.com>
2022-08-16 05:50:31 +0500

    Add Styling to Rentals, Add a Rental, Add a Car

--- src/pages/AddCar.jsx
+++ src/pages/AddCar.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
+import './AddCar.css';
 
 function AddCar() {
   return (
     <div className="addCar">
       <h2>Add Car</h2>
       <form className="addCarForm">
-        <input type="text" placeholder="Brand" name="name" required />
+        <input type="text" placeholder="City" name="name" className="form-control form-control-lg" aria-label=".form-control-lg example" required />
         <br />
-        <input type="text" placeholder="Model" name="model" required />
+        <input type="text" placeholder="Model" name="model" className="form-control form-control-lg" aria-label=".form-control-lg" required />
         <br />
-        <input type="text" placeholder="Image Url" name="image" required />
+        <input type="text" placeholder="Image Url" name="image" className="form-control form-control-lg" aria-label=".form-control-lg" required />
         <br />
-        <input type="textarea" placeholder="Description" name="desc" required />
+        <input type="textarea" placeholder="Description" name="desc" className="form-control form-control-lg" aria-label=".form-control-lg" required />
         <br />
-        <input type="number" min="0.00" step="10.00" placeholder="Rent Cost" name="rent" required />
+        <input type="number" min="0.00" step="10.00" placeholder="Rent Cost" name="rent" className="form-control form-control-lg" aria-label=".form-control-lg" required />
         <br />
-        <button type="submit">Add Car</button>
+        <button id="btn-add-car" type="submit">Add Car</button>
       </form>
     </div>
   );
