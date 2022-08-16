a704f7367ecf6d965924319cde0c4cb89ceba9fa
J. Luis Arredondo <jose.arredondo.a@outlook.com>
2022-08-16 09:54:53 -0500

    Add 'POST car' to local API

--- src/pages/AddCar.jsx
+++ src/pages/AddCar.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { addCar } from '../redux/car/car';
 
 function AddCar() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const obj = {
+      name: form.name.value,
+      model: form.model.value,
+      image: form.image.value,
+      desc: form.desc.value,
+      rent: form.rent.value,
+    };
+    dispatch(addCar(obj));
+    navigate('/');
+  };
+
   return (
     <div className="addCar">
       <h2>Add Car</h2>
-      <form className="addCarForm">
+      <form className="addCarForm" onSubmit={handleSubmit}>
         <input type="text" placeholder="Brand" name="name" required />
         <br />
         <input type="text" placeholder="Model" name="model" required />
--- src/redux/car/car.js
+++ src/redux/car/car.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const baseUrl = 'http://localhost:3000/api/v1/';
 const FETCH_CARS = 'FETCH_CARS';
 const FETCH_CAR_DETAILS = 'FETCH_CAR_DETAILS';
-// const ADD_CAR = 'ADD_CAR';
+const ADD_CAR = 'ADD_CAR';
 // const DELETE_CAR = 'DELETE_CAR';
 const initialState = [];
 
@@ -23,14 +23,22 @@ export const fetchCarDetails = (id) => async (dispatch) => {
   });
 };
 
+export const addCar = (car) => async (dispatch) => {
+  const res = await axios.post(`${baseUrl}cars`, car);
+  dispatch({
+    type: ADD_CAR,
+    payload: res.data,
+  });
+};
+
 const reducer = (state = initialState, action = {}) => {
   switch (action.type) {
     case 'FETCH_CARS':
       return action.payload;
     case 'FETCH_CAR_DETAILS':
       return action.payload;
-    // case 'ADD_CAR':
-    //   return [...state, action.payload];
+    case 'ADD_CAR':
+      return [...state, action.payload];
     // case 'DELETE_CAR':
     //   return state.filter((car) => car.id !== action.element);
     default:
