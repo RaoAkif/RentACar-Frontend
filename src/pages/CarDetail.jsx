49c51f54e03ccf7008d0fd0e22a44e5804c80f2e
J. Luis Arredondo <jose.arredondo.a@outlook.com>
2022-08-16 09:47:12 -0500

    Add 'fetchCarDetails' from store and local API

--- src/pages/CarDetail.jsx
+++ src/pages/CarDetail.jsx
@@ -1,25 +1,28 @@
-import React from 'react';
-// import { useParams } from "react-router-dom";
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { fetchCarDetails } from '../redux/car/car';
 
 function CarDetail() {
-  // const { id } = useParams();
-  const name = 'BMW';
-  const model = 'M3';
-  const rent = '$100';
-  const image = 'https://th.bing.com/th/id/R.e8d460c004d696c3255d513978bfb864?rik=4bBYE9ASyq7lWg&riu=http%3a%2f%2fwww.marinoperformancemotors.com%2fimagetag%2f12652%2f37%2fl%2fUsed-2018-BMW-M3.jpg&ehk=RTlfzS94QNQ836EwJMu3p4xkRWqZMfkGTEo%2b99cBmgs%3d&risl=&pid=ImgRaw&r=0';
-  const desc = 'This is a description of the car';
+  const dispatch = useDispatch();
+  const { id } = useParams();
+  const car = useSelector((store) => store.car);
+
+  useEffect(() => {
+    dispatch(fetchCarDetails(id));
+  }, [dispatch]);
 
   return (
     <div className="carDetail">
       <div>
-        <h2>{name}</h2>
-        <p className="carModel">{model}</p>
+        <h2>{car.name}</h2>
+        <p className="carModel">{car.model}</p>
       </div>
       <div className="carInfoGroup">
-        <div className="carImage"><img src={image} alt={name + model} /></div>
+        <div className="carImage"><img src={car.image} alt={car.name + car.model} /></div>
         <div className="carInfo">
-          <p className="carPrice">{rent}</p>
-          <p className="carDesc">{desc}</p>
+          <p className="carPrice">{car.rent}</p>
+          <p className="carDesc">{car.desc}</p>
 
           <button className="rentBtn" type="button">Rent</button>
         </div>
--- src/redux/car/car.js
+++ src/redux/car/car.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const baseUrl = 'http://localhost:3000/api/v1/';
 const FETCH_CARS = 'FETCH_CARS';
+const FETCH_CAR_DETAILS = 'FETCH_CAR_DETAILS';
 // const ADD_CAR = 'ADD_CAR';
 // const DELETE_CAR = 'DELETE_CAR';
 const initialState = [];
@@ -14,9 +15,19 @@ export const fetchCars = () => async (dispatch) => {
   });
 };
 
+export const fetchCarDetails = (id) => async (dispatch) => {
+  const res = await axios.get(`${baseUrl}cars/${id}`);
+  dispatch({
+    type: FETCH_CAR_DETAILS,
+    payload: res.data,
+  });
+};
+
 const reducer = (state = initialState, action = {}) => {
   switch (action.type) {
     case 'FETCH_CARS':
+      return action.payload;
+    case 'FETCH_CAR_DETAILS':
       return action.payload;
     // case 'ADD_CAR':
     //   return [...state, action.payload];
