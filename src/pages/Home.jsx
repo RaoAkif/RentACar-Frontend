d45b8e3d0689529d547c2cc1849d7c9a9270e360
raoakif <akifrao@gmail.com>
2022-08-18 02:52:18 +0500

    Add CarsList to the Home Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,68 @@
 import React from 'react';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import CarCard from '../components/CarCard';
 import './Home.css';
 
 function Home() {
+  const cars = [
+    {
+      carName: 'Audi', model: 'A4', price: '$200', image: 'https://raw.githubusercontent.com/CodeBitChips/Images/main/cars/bmw-m3.png', city: 'New York', date: '12/12/2018',
+    },
+    {
+      carName: 'Ford', model: 'Fiesta', price: '$100', image: 'https://raw.githubusercontent.com/CodeBitChips/Images/main/cars/bmw-m3.png', city: 'New York', date: '12/12/2018',
+    },
+    {
+      carName: 'Ford', model: 'Fiesta', price: '$100', image: 'https://raw.githubusercontent.com/CodeBitChips/Images/main/cars/bmw-m3.png', city: 'New York', date: '12/12/2018',
+    },
+    {
+      carName: 'Ford', model: 'Fiesta', price: '$100', image: 'https://raw.githubusercontent.com/CodeBitChips/Images/main/cars/bmw-m3.png', city: 'New York', date: '12/12/2018',
+    },
+    {
+      carName: 'Ford', model: 'Fiesta', price: '$100', image: 'https://raw.githubusercontent.com/CodeBitChips/Images/main/cars/bmw-m3.png', city: 'New York', date: '12/12/2018',
+    },
+    {
+      carName: 'Ford', model: 'Fiesta', price: '$100', image: 'https://raw.githubusercontent.com/CodeBitChips/Images/main/cars/bmw-m3.png', city: 'New York', date: '12/12/2018',
+    },
+  ];
+
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 1,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 1,
+    },
+    laptop: {
+      breakpoint: { max: 1024, min: 768 },
+      items: 1,
+    },
+    tablet: {
+      breakpoint: { max: 768, min: 464 },
+      items: 1,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
+
   return (
-    <div className="home-page-div">
-      <h1>Home</h1>
-    </div>
+    <Carousel responsive={responsive} className="carousel-container" infinite>
+      {cars.map((car) => (
+        <CarCard
+          key={car.id}
+          carName={car.carName}
+          model={car.model}
+          price={car.price}
+          image={car.image}
+          city={car.city}
+          date={car.date}
+        />
+      ))}
+    </Carousel>
   );
 }
 
