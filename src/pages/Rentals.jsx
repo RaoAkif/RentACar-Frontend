3e243eb12a327a3f11d1d4b06ff30045797db8b7
raoakif <akifrao@gmail.com>
2022-08-18 01:53:18 +0500

    Add React Multi Carousel to the Rentals Page

--- src/pages/Rentals.jsx
+++ src/pages/Rentals.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 import CarCard from '../components/CarCard';
 import './Rentals.css';
 
@@ -10,25 +12,57 @@ function Rentals() {
     {
       carName: 'Ford', model: 'Fiesta', price: '$100', image: 'https://raw.githubusercontent.com/CodeBitChips/Images/main/cars/bmw-m3.png', city: 'New York', date: '12/12/2018',
     },
+    {
+      carName: 'Ford', model: 'Fiesta', price: '$100', image: 'https://raw.githubusercontent.com/CodeBitChips/Images/main/cars/bmw-m3.png', city: 'New York', date: '12/12/2018',
+    },
+    {
+      carName: 'Ford', model: 'Fiesta', price: '$100', image: 'https://raw.githubusercontent.com/CodeBitChips/Images/main/cars/bmw-m3.png', city: 'New York', date: '12/12/2018',
+    },
+    {
+      carName: 'Ford', model: 'Fiesta', price: '$100', image: 'https://raw.githubusercontent.com/CodeBitChips/Images/main/cars/bmw-m3.png', city: 'New York', date: '12/12/2018',
+    },
+    {
+      carName: 'Ford', model: 'Fiesta', price: '$100', image: 'https://raw.githubusercontent.com/CodeBitChips/Images/main/cars/bmw-m3.png', city: 'New York', date: '12/12/2018',
+    },
   ];
 
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 1,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 1,
+    },
+    laptop: {
+      breakpoint: { max: 1024, min: 768 },
+      items: 1,
+    },
+    tablet: {
+      breakpoint: { max: 768, min: 464 },
+      items: 1,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
+
   return (
-    <div>
-      <h2 className="page-title">Rentals</h2>
-      <div className="rentals">
-        {rents.map((rent) => (
-          <CarCard
-            key={rent.id}
-            carName={rent.carName}
-            model={rent.model}
-            price={rent.price}
-            image={rent.image}
-            city={rent.city}
-            date={rent.date}
-          />
-        ))}
-      </div>
-    </div>
+    <Carousel responsive={responsive} className="carousel-container" infinite>
+      {rents.map((rent) => (
+        <CarCard
+          key={rent.id}
+          carName={rent.carName}
+          model={rent.model}
+          price={rent.price}
+          image={rent.image}
+          city={rent.city}
+          date={rent.date}
+        />
+      ))}
+    </Carousel>
   );
 }
 
