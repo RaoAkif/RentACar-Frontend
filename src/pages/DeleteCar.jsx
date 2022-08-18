711229739d9213a30d8d15686d7df2a9a2697890
Roberto Andres Condezo Monge <ra.condezomonge@gmail.com>
2022-08-18 09:03:52 -0500

    Change deleteCarCard component instead of carCard component

--- src/pages/DeleteCar.jsx
+++ src/pages/DeleteCar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import CarCard from '../components/CarCard';
+import DeleteCarCard from '../components/DeleteCarCard';
 import './DeleteCar.css';
 
 function DeleteCar() {
@@ -10,15 +10,21 @@ function DeleteCar() {
     {
       name: 'Audi', model: 'A4', rent: '$100', image: 'https://raw.githubusercontent.com/CodeBitChips/Images/main/cars/bmw-m3.png', desc: 'This is a description of the car',
     },
+    {
+      name: 'BMW', model: 'M3', rent: '$100', image: 'https://raw.githubusercontent.com/CodeBitChips/Images/main/cars/bmw-m3.png', desc: 'This is a description of the car',
+    },
+    {
+      name: 'Audi', model: 'A4', rent: '$100', image: 'https://raw.githubusercontent.com/CodeBitChips/Images/main/cars/bmw-m3.png', desc: 'This is a description of the car',
+    },
   ];
 
   return (
     <div className="delete-car-main">
-      <h2>Delete Car</h2>
-      <form className="carsDelCards">
+      <h2>DELETE&nbsp;&nbsp;CAR</h2>
+      <form className="d-cars-delete-cards">
         {cars.map((car) => (
-          <div className="carDelCard" key={car.name}>
-            <CarCard
+          <div className="d-car-delete-card" key={car.name}>
+            <DeleteCarCard
               key={car.id}
               name={car.name}
               model={car.model}
@@ -26,10 +32,9 @@ function DeleteCar() {
               image={car.image}
               desc={car.desc}
             />
-            <input type="checkbox" name="delCar" id={car.name} />
           </div>
         ))}
-        <button type="submit">Delete</button>
+        <button className="delete-btn" type="submit">Delete</button>
       </form>
     </div>
   );
