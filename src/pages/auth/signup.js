c0c4d73ab60a2517091feedc9d904eb5b8754098
fasaldana <fernando_s_98@hotmail.com>
2022-08-18 13:31:50 -0500

    Add signup functionality

--- src/pages/auth/signup.js
+++ src/pages/auth/signup.js
@@ -1,26 +1,13 @@
 import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { createUser } from '../../redux/user/user';
 
 export default function signup() {
-  const { status } = useSelector((state) => state.user);
   const [name, setName] = useState('');
   const dispatch = useDispatch();
   const handleSubmit = (e) => {
     e.preventDefault();
     dispatch(createUser(name));
-    if (status === 'succeeded') {
-      window.location.href = '/login';
-    } else {
-      const dialog = document.createElement('dialog');
-      dialog.classList.add('dialog');
-      dialog.innerHTML = `user ${name} already exists`;
-      document.body.appendChild(dialog);
-      dialog.showModal();
-      setTimeout(() => {
-        dialog.close();
-      }, 2000);
-    }
   };
   return (
     <div className="auth-page">
