4770dcf04a7d9f31aaa2c1acbd66905d2d2d093c
Rao Muhammad Akif Tufail <akifrao@gmail.com>
2022-08-20 02:38:40 +0500

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,35 @@
 import React, { useEffect } from 'react';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchCars } from '../redux/car/car';
 import CarCard from '../components/CarCard';
 import './Home.css';
 
 function Home() {
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 1,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 1,
+    },
+    laptop: {
+      breakpoint: { max: 1024, min: 768 },
+      items: 1,
+    },
+    tablet: {
+      breakpoint: { max: 768, min: 464 },
+      items: 1,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
+
   const dispatch = useDispatch();
   const cars = useSelector((store) => store.car);
 
@@ -14,22 +39,26 @@ function Home() {
 
   const emptyCarsPage = <h2>There are no cars available</h2>;
 
-  const carsPage = cars.map((car) => (
-    <div key={car.id}>
-      <a href={`/car_detail/${car.id}`}>
-        <CarCard
-          key={car.id}
-          carName={car.name}
-          model={car.model}
-          price={car.rent}
-          image={car.image}
-          city={car.city}
-          date={car.date}
-        />
-      </a>
-    </div>
-  ));
-
+  const carsPage = (
+    <Carousel responsive={responsive} className="carousel-container" infinite>
+      cars.map((car) => (
+        <div key={car.id}>
+          <a href={`/car_detail/${car.id}`}>
+            <CarCard
+              key={car.id}
+              carName={car.name}
+              model={car.model}
+              price={car.rent}
+              image={car.image}
+              city={car.city}
+              date={car.date}
+            />
+          </a>
+        </div>
+      ))
+    </Carousel>
+  )
+  
   if (cars.length === 0) {
     return emptyCarsPage;
   }
