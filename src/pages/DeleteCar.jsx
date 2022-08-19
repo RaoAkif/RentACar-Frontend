2baf48be46f0d9df3eaf2940742a961c5167162c
Roberto Andres Condezo Monge <ra.condezomonge@gmail.com>
2022-08-19 10:52:06 -0500

    Add a Table header

--- src/pages/DeleteCar.jsx
+++ src/pages/DeleteCar.jsx
@@ -5,16 +5,16 @@ import './DeleteCar.css';
 function DeleteCar() {
   const cars = [
     {
-      name: 'BMW', model: 'M3', rent: '$100', image: 'https://raw.githubusercontent.com/CodeBitChips/Images/main/cars/bmw-m3.png', desc: 'This is a description of the car',
+      id: '1', name: 'BMW', model: 'M3', rent: '$100', image: 'https://raw.githubusercontent.com/CodeBitChips/Images/main/cars/bmw-m3.png', desc: 'This is a description of the car',
     },
     {
-      name: 'Audi', model: 'A4', rent: '$100', image: 'https://raw.githubusercontent.com/CodeBitChips/Images/main/cars/bmw-m3.png', desc: 'This is a description of the car',
+      id: '2', name: 'Audi', model: 'A4', rent: '$100', image: 'https://raw.githubusercontent.com/CodeBitChips/Images/main/cars/bmw-m3.png', desc: 'This is a description of the car',
     },
     {
-      name: 'BMW', model: 'M3', rent: '$100', image: 'https://raw.githubusercontent.com/CodeBitChips/Images/main/cars/bmw-m3.png', desc: 'This is a description of the car',
+      id: '3', name: 'BMW', model: 'M3', rent: '$100', image: 'https://raw.githubusercontent.com/CodeBitChips/Images/main/cars/bmw-m3.png', desc: 'This is a description of the car',
     },
     {
-      name: 'Audi', model: 'A4', rent: '$100', image: 'https://raw.githubusercontent.com/CodeBitChips/Images/main/cars/bmw-m3.png', desc: 'This is a description of the car',
+      id: '4', name: 'Audi', model: 'A4', rent: '$100', image: 'https://raw.githubusercontent.com/CodeBitChips/Images/main/cars/bmw-m3.png', desc: 'This is a description of the car',
     },
   ];
 
@@ -22,18 +22,24 @@ function DeleteCar() {
     <div className="delete-car-main">
       <h2>DELETE&nbsp;&nbsp;CAR</h2>
       <form className="d-cars-delete-cards">
-        {cars.map((car) => (
-          <div className="d-car-delete-card" key={car.name}>
-            <DeleteCarCard
-              key={car.id}
-              name={car.name}
-              model={car.model}
-              rent={car.rent}
-              image={car.image}
-              desc={car.desc}
-            />
-          </div>
-        ))}
+        <table className="table table-striped table-bordered delete-table">
+          <thead className="table-dark">
+            <tr>
+              <th className="th-checkbox" scope="col"> </th>
+              <th scope="col">Company</th>
+              <th scope="col">Model</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cars.map((car) => (
+              <DeleteCarCard
+                key={car.id}
+                name={car.name}
+                model={car.model}
+              />
+            ))}
+          </tbody>
+        </table>
         <button className="delete-btn" type="submit">Delete</button>
       </form>
     </div>
