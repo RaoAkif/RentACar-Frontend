97ec4442463b5305a6a8e7835f42852bbdb20ffb
J. Luis Arredondo <jose.arredondo.a@outlook.com>
2022-08-24 16:13:55 -0500

    Add 'CarCard' base test

---
+++ src/__tests__/CarCard.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { render } from '@testing-library/react';
+import store from '../redux/configureStore';
+import CarCard from '../components/CarCard';
+
+describe("CarCard", () => {
+  const car = { id: 1, name: 'Audi', price: '100', image: 'https://www.audi.com/content/dam/audi/us/en/all-models/a4/a4-allroad-quattro/2020/models-a4-allroad-quattro-2020-hero.jpg/_jcr_content/renditions/cq5dam.web.1200.675.jpeg' };
+
+  it("should render correctly", () => {
+    const { container } = render(
+      <BrowserRouter>
+        <Provider store={store}>
+          <CarCard />
+        </Provider>
+        ,
+      </BrowserRouter>,
+    );
+
+    expect(container).toMatchSnapshot();
+  });
+});
