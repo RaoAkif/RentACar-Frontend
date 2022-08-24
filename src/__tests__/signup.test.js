f4c2c086e62d40e380b4271ccc7a361a53c3da01
J. Luis Arredondo <jose.arredondo.a@outlook.com>
2022-08-24 16:05:56 -0500

    Add 'signup' base test

---
+++ src/__tests__/signup.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from '../redux/configureStore';
+import { render } from '@testing-library/react';
+import Signup from '../pages/auth/signup';
+
+describe('Signup', () => {  
+  it('should render correctly', () => {
+    const { container } = render(
+      <BrowserRouter>
+        <Provider store={store}>
+          <Signup />
+        </Provider>,
+      </BrowserRouter>,
+    );
+
+    expect(container).toMatchSnapshot();
+  });
+});
