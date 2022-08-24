2537cbee961c25f7128a17a433c9aadd295f231d
J. Luis Arredondo <jose.arredondo.a@outlook.com>
2022-08-24 16:17:14 -0500

    Modify 'CarCard' to pass props

--- src/__tests__/CarCard.test.js
+++ src/__tests__/CarCard.test.js
@@ -6,13 +6,18 @@ import store from '../redux/configureStore';
 import CarCard from '../components/CarCard';
 
 describe("CarCard", () => {
-  const car = { id: 1, name: 'Audi', price: '100', image: 'https://www.audi.com/content/dam/audi/us/en/all-models/a4/a4-allroad-quattro/2020/models-a4-allroad-quattro-2020-hero.jpg/_jcr_content/renditions/cq5dam.web.1200.675.jpeg' };
+  const car = { id: 1, name: 'Audi', model: 'R3', price: '100', image: 'https://www.audi.com/content/dam/audi/us/en/all-models/a4/a4-allroad-quattro/2020/models-a4-allroad-quattro-2020-hero.jpg/_jcr_content/renditions/cq5dam.web.1200.675.jpeg' };
 
   it("should render correctly", () => {
     const { container } = render(
       <BrowserRouter>
         <Provider store={store}>
-          <CarCard />
+          <CarCard
+            carName = {car.name}
+            model = {car.model}
+            price = {car.price}
+            image = {car.image}
+          />
         </Provider>
         ,
       </BrowserRouter>,
