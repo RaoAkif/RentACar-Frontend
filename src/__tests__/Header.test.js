18be2c10022081cb3a63484c5e97b8d6227d7502
J. Luis Arredondo <jose.arredondo.a@outlook.com>
2022-08-24 16:24:44 -0500

    Add 'Header' base test

---
+++ src/__tests__/Header.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { render } from '@testing-library/react';
+import store from '../redux/configureStore';
+import Header from '../components/Header';
+
+describe("Header", () => {
+  it("should render correctly", () => {
+    const { container } = render(
+      <BrowserRouter>
+        <Provider store={store}>
+          <Header />
+        </Provider>
+        ,
+      </BrowserRouter>,
+    );
+
+    expect(container).toMatchSnapshot();
+  });
+});
