82f4ad66a65d453d683f0e3f99051a20b2079517
J. Luis Arredondo <jose.arredondo.a@outlook.com>
2022-08-24 10:29:34 -0500

    Add 'AddCar' base test

---
+++ src/__tests__/AddCar.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from '../redux/configureStore';
+import { render } from '@testing-library/react';
+import AddCar from '../pages/AddCar';
+
+describe('AddCar page', () => {
+  it('should render correctly', () => {
+    const { container } = render(
+      <BrowserRouter>
+        <Provider store={store}>
+          <AddCar />
+        </Provider>,
+      </BrowserRouter>,
+    );
+
+    expect(container).toMatchSnapshot();
+  });
+});
